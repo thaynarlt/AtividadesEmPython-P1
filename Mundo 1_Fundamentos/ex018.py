66e2cd40901d12e14cb43cb38b2d9538c3ebd39a
Thayná Lopes <75785465+thaynarlt@users.noreply.github.com>
2023-02-04 08:11:27 -0300

    Seno, Cosseno e Tangente

--- Mundo 1_Fundamentos/ex018.py
+++ Mundo 1_Fundamentos/ex018.py
@@ -2,3 +2,4 @@
 import math
 num = float(input('Digite o valor de um ângulo qualquer'))
 print('O seno é {:.2f}, o cosseno é {:.2f} e a tangente é {:.2f}'.format(math.sin(math.radians(num)), math.cos(math.radians(num)), math.tan(math.radians(num))))
+

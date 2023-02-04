1a6430a8aa0fc2de0a8826303d415ae0878570e2
Thayná Lopes <75785465+thaynarlt@users.noreply.github.com>
2023-02-04 08:15:02 -0300

    Substituindo arquivo de áudio por versão comprimida

--- Mundo 1_Fundamentos/ex021.py
+++ Mundo 1_Fundamentos/ex021.py
@@ -1,1 +1,6 @@
-#Faça um programa em Python que abra e reproduza o áudio de arquivo MP3
+#Faça um prograimport pygame
+pygame.mixer.init()
+pygame.init()
+pygame.mixer.music.load('midnightcity.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()

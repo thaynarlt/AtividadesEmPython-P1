3e2e7b8da15a599dda98ace67a03f7d9ad8918d5
Thayná Tolentino <75785465+thaynarlt@users.noreply.github.com>
2023-06-01 08:38:30 -0300

    Frase toda em letras maiúsculas

---
+++ Atividades APE/ex67.py
@@ -0,0 +1,5 @@
+#3. Faça um programa que leia uma frase e a exiba toda em letras maiúsculas.
+
+frase = input('Digite uma frase:')
+
+print(f'A frase digitada com todas as letras maiúsculas: {frase.upper()}')

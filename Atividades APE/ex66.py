3fed38b7c740d21b3433b110a8a3d5270ab5b2c9
Thayná Tolentino <75785465+thaynarlt@users.noreply.github.com>
2023-06-01 08:38:05 -0300

    Frase sem os espaços em branco

---
+++ Atividades APE/ex66.py
@@ -0,0 +1,6 @@
+#2. Faça um programa que leia uma frase e a exiba sem os espaços em branco.
+frase = input("Digite uma frase: ")
+
+frase_sem_espacos = frase.replace(" ", "") #vai retirar os espaços brancos 
+
+print("Frase sem espaços em branco:", frase_sem_espacos)

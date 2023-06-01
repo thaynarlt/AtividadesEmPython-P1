d3f9ac1aea8cc5fdda7b1f49cef36ef436a9b2bc
Thayná Tolentino <75785465+thaynarlt@users.noreply.github.com>
2023-06-01 08:38:53 -0300

    Inicial de cada palavra em maiúsculo

---
+++ Atividades APE/ex68.py
@@ -0,0 +1,5 @@
+#4. Faça um programa que leia uma frase e a exiba com a inicial de cada palavra em maiúsculo.
+
+frase = input('Digite uma frase:')
+
+print(f'A frase digitada com todas as letras maiúsculas: {frase.title()}')

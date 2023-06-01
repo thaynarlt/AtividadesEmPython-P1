468e0f1543d606f230f27262afa3638f372b47ec
Thayná Tolentino <75785465+thaynarlt@users.noreply.github.com>
2023-06-01 08:39:43 -0300

    Frase e a exiba invertida

---
+++ Atividades APE/ex70.py
@@ -0,0 +1,7 @@
+#6. Faça um programa que leia uma frase e a exiba invertida.
+
+frase = input('Digite uma frase:')
+
+frase_invertida = frase[::-1] #vai inverter a frase
+
+print("Frase invertida:", frase_invertida)

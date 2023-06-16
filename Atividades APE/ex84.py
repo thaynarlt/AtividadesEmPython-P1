5905bd8d2ac042341f714be499d1d1fe8283728f
Thayná Tolentino <75785465+thaynarlt@users.noreply.github.com>
2023-06-16 09:16:08 -0300

    Vetor, médias, leitura DEF

--- Atividades APE/ex84.py
+++ Atividades APE/ex84.py
@@ -1,18 +1,44 @@
-#2. Escreva uma função chamada primo para determinar se um número é primo ou não. A
-#função deve receber um número inteiro, retornar True se o número for primo, ou False caso contrário.
-#Escreva também um programa que, usando a função primo criada, exiba os números primos entre 1 e 100.
+#3. Escreva um programa que leia N números inteiros (máximo 20) e armazene em um
+#vetor X. Calcule a média dos elementos do vetor e informe quantos elementos estão
+#abaixo da média do conjunto.
+#Crie as seguintes funções:
 
-def primo(numero):
-    if numero > 1: #se o numero é maior que 1
-        for i in range(2, numero):
-            if numero % i == 0: #se o numero divido por i == 0 então
-                print(f"O número {numero} não é primo") # o número não é primo
-                break
-        else:
-            print(f"O número {numero} é primo") # o número é primo
-            return True #vai 'receber' o True se for primo
-    else:
-        return False #vai 'receber' o False se não for primo
+#• Uma função que faça a leitura dos elementos de um vetor de inteiros. Essa função
+#deve receber como parâmetro o número de elementos do vetor e deve retornar o vetor preenchido.
 
-for num in range(2, 101): #vai percorrer os nmrs de 2 a 101
-    primo(num)
+def leitura_elementos(num_elements):
+    vetor=[None] * num_elements 
+    for i in range(num_elements):
+        vetor[i]=int(input('Adicione um número ao seu vetor:'))
+    print(vetor)
+    return vetor
+
+
+num=int(input('Digite a quantidade de números em seu vetor: '))
+vetor_preenchido =leitura_elementos(num)  
+#• Uma função que calcule a média dos elementos de um vetor. Essa função deve
+#receber como parâmetro um vetor e retornar a média dos elementos dele.
+
+def media_vetor(vetor):
+    soma = sum(vetor)
+    quantd = len(vetor)
+
+    media = soma/quantd
+    print(f'A média do vetor é: {media}')
+    return media
+
+media =media_vetor(vetor_preenchido)
+
+
+#• Uma função que receba um vetor e um número, e retorne quantos elementos do
+#vetor estão abaixo desse número.
+
+def receber(vetor):
+    cont=0
+    num = int(input('Digite um número a ser conferido: '))
+    for i in range(len(vetor)):
+        if num < vetor[i]:
+            cont+=1
+    print(f'O número digitado foi {num} e {cont} elemento(s) está(ão) abaixo do número digitado.')
+
+receber(vetor_preenchido)
